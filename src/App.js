f4ed8a901812e7d54ab3764454a4e2e1c831b547
Sam Sheets <sam.sheets94@gmail.com>
2020-12-11 16:19:14 -0700

    implemented search field and button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import React, {Component} from 'react';
 
-import './App.css';
+class  App extends Component {
+  state = {
+    artistQuery: ''
+  };
 
-function App() {
-  return (
-    <div className="App">
-      A React App
-    </div>
-  );
+  updateArtistQuery = (event) => {
+    console.log('event.target.value', event.target.value)
+    this.setState({
+      artistQuery: event.target.value
+    })
+  }
+
+  searchArtist = () => {
+    console.log('this.state', this.state)
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.searchArtist();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Music Master</h2>
+        <input 
+          onChange={this.updateArtistQuery}
+          onKeyPress={this.handleKeyPress}
+          placeholder='Search for an Artist' />
+        <button onClick={this.searchArtist}>Search</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
